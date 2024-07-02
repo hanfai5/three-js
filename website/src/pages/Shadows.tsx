b395aaa59d2c433b9484f525e894e30b18d382fd
Han <hfai97@gmail.com>
2024-07-03 07:04:07 +0800

    Added the Baked Shadow section for the website

--- website/src/pages/Shadows.tsx
+++ website/src/pages/Shadows.tsx
@@ -1,12 +1,76 @@
 import { OrbitControls } from "@react-three/drei";
-import { Canvas, useThree } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
-import { CameraHelper, DirectionalLight, PointLight, SpotLight } from "three";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import {
+  CameraHelper,
+  DirectionalLight,
+  Mesh,
+  PointLight,
+  SpotLight,
+  TextureLoader,
+} from "three";
 import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
 
-const Sphere = ({ position }: { position: [number, number, number] }) => {
+const bakedShadowImagePath: string = "/public/textures/shadows/bakedShadow.jpg";
+const simpleShadowImagePath: string =
+  "/public/textures/shadows/simpleShadow.jpg";
+
+const SimpleShadow = ({
+  position,
+  rotation,
+  opacity,
+}: {
+  position: [number, number, number];
+  rotation: [number, number, number];
+  opacity: number;
+}) => {
+  const [simpleShadowTexture] = useLoader(TextureLoader, [
+    simpleShadowImagePath,
+  ]);
+
   return (
-    <mesh position={position} castShadow>
+    <mesh position={position} rotation={rotation}>
+      <planeGeometry args={[1.5, 1.5]} />
+      <meshBasicMaterial
+        color={0x000000}
+        transparent
+        alphaMap={simpleShadowTexture}
+        opacity={opacity}
+      />
+    </mesh>
+  );
+};
+
+const Sphere = ({
+  position,
+  animate,
+  setShadowPosition,
+  setShadowOpacity,
+}: {
+  position: [number, number, number];
+  animate?: boolean;
+  setShadowPosition: Dispatch<SetStateAction<[number, number, number]>>;
+  setShadowOpacity: Dispatch<SetStateAction<number>>;
+}) => {
+  const ref = useRef<Mesh>(null!);
+
+  useFrame(({ clock }) => {
+    if (!animate) return;
+    ref.current.position.x = Math.cos(clock.getElapsedTime()) * 1.5;
+    ref.current.position.y = Math.abs(Math.sin(clock.getElapsedTime() * 3));
+    ref.current.position.z = Math.sin(clock.getElapsedTime()) * 1.5;
+
+    setShadowPosition([
+      ref.current.position.x,
+      -0.5 + 0.01,
+      ref.current.position.z,
+    ]);
+
+    setShadowOpacity((1 - ref.current.position.y) * 0.5);
+  });
+
+  return (
+    <mesh ref={ref} position={position} castShadow>
       <sphereGeometry args={[0.5, 32, 32]} />
       <meshStandardMaterial roughness={0.7} />
     </mesh>
@@ -16,14 +80,20 @@ const Sphere = ({ position }: { position: [number, number, number] }) => {
 const Plane = ({
   position,
   rotation,
+  bakedShadow,
 }: {
   position: [number, number, number];
   rotation: [number, number, number];
+  bakedShadow?: boolean;
 }) => {
+  const ref = useRef<Mesh>(null!);
+  const [bakedShadowTexture] = useLoader(TextureLoader, [bakedShadowImagePath]);
+
   return (
-    <mesh position={position} rotation={rotation} receiveShadow>
+    <mesh ref={ref} position={position} rotation={rotation} receiveShadow>
       <planeGeometry args={[5, 5]} />
-      <meshStandardMaterial roughness={0.7} />
+      {!bakedShadow && <meshStandardMaterial roughness={0.7} />}
+      {bakedShadow && <meshBasicMaterial map={bakedShadowTexture} />}
     </mesh>
   );
 };
@@ -142,8 +212,15 @@ const PointLightWithHelper = () => {
 
 const Shadows = () => {
   const spherePosition: [number, number, number] = [0, 0, 0];
-  const planePosition: [number, number, number] = [0, -0.65, 0];
+  const planePosition: [number, number, number] = [0, -0.5, 0];
   const planeRotation: [number, number, number] = [-Math.PI * 0.5, 0, 0];
+  const shadowRotation: [number, number, number] = [-Math.PI * 0.5, 0, 0];
+  const [shadowPosition, setShadowPosition] = useState<
+    [number, number, number]
+  >([planePosition[0], planePosition[1] + 0.01, planePosition[2]]);
+  const [shadowOpacity, setShadowOpacity] = useState<number>(
+    1 - spherePosition[1]
+  );
 
   return (
     <section className="max-w-[1200px] mx-auto flex flex-col gap-y-4">
@@ -152,7 +229,12 @@ const Shadows = () => {
       <h2 className="subtitle">Directional Light</h2>
       <Canvas style={{ height: "400px" }} shadows>
         <OrbitControls enablePan={false} enableRotate={true} />
-        <Sphere position={spherePosition} />
+        <Sphere
+          position={spherePosition}
+          animate={false}
+          setShadowPosition={setShadowPosition}
+          setShadowOpacity={setShadowOpacity}
+        />
         <Plane position={planePosition} rotation={planeRotation} />
         <ambientLight color={0xffffff} intensity={1} />
         <DirectionalLightWithHelper />
@@ -161,7 +243,11 @@ const Shadows = () => {
       <h2 className="subtitle">Spot Light</h2>
       <Canvas style={{ height: "400px" }} shadows>
         <OrbitControls enablePan={false} enableRotate={true} />
-        <Sphere position={spherePosition} />
+        <Sphere
+          position={spherePosition}
+          setShadowPosition={setShadowPosition}
+          setShadowOpacity={setShadowOpacity}
+        />
         <Plane position={planePosition} rotation={planeRotation} />
         <ambientLight color={0xffffff} intensity={1} />
         <SpotLightWithHelper />
@@ -170,10 +256,48 @@ const Shadows = () => {
       <h2 className="subtitle">Point Light</h2>
       <Canvas style={{ height: "400px" }} shadows>
         <OrbitControls enablePan={false} enableRotate={true} />
-        <Sphere position={spherePosition} />
+        <Sphere
+          position={spherePosition}
+          setShadowPosition={setShadowPosition}
+          setShadowOpacity={setShadowOpacity}
+        />
         <Plane position={planePosition} rotation={planeRotation} />
         <ambientLight color={0xffffff} intensity={1} />
         <PointLightWithHelper />
+      </Canvas>
+
+      <h2 className="subtitle">Baked Shadow</h2>
+      <Canvas style={{ height: "400px" }} shadows>
+        <OrbitControls enablePan={false} enableRotate={true} />
+        <Sphere
+          position={spherePosition}
+          setShadowPosition={setShadowPosition}
+          setShadowOpacity={setShadowOpacity}
+        />
+        <Plane
+          position={planePosition}
+          rotation={planeRotation}
+          bakedShadow={true}
+        />
+        <ambientLight color={0xffffff} intensity={1} />
+      </Canvas>
+
+      <h2 className="subtitle">Simple Shadow</h2>
+      <Canvas style={{ height: "400px" }} shadows>
+        <OrbitControls enablePan={false} enableRotate={true} />
+        <Sphere
+          position={spherePosition}
+          animate={true}
+          setShadowPosition={setShadowPosition}
+          setShadowOpacity={setShadowOpacity}
+        />
+        <Plane position={planePosition} rotation={planeRotation} />
+        <SimpleShadow
+          position={shadowPosition}
+          rotation={shadowRotation}
+          opacity={shadowOpacity}
+        />
+        <ambientLight color={0xffffff} intensity={1.5} />
       </Canvas>
     </section>
   );
