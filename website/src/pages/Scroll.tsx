b30b80cd0aa2810f3feb31f334f4fd4694ac8917
Han <hfai97@gmail.com>
2024-07-10 18:29:58 +0800

    Added the mesh Toom material for the meshed and adjusted the y positoning

--- website/src/pages/Scroll.tsx
+++ website/src/pages/Scroll.tsx
@@ -1,16 +1,88 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { NearestFilter, Texture, TextureLoader } from "three";
 
-const Box = () => {
+const parameters = {
+  materialColor: "#ffeded",
+};
+
+type MeshProps = {
+  gradientTexture?: Texture;
+  position?: [number, number, number];
+};
+
+const Mesh1: React.FC<MeshProps> = ({
+  gradientTexture,
+  position = [0, 0, 0],
+}) => {
   return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color={"red"} />
+    <mesh position={position}>
+      <torusGeometry args={[1, 0.4, 16, 60]} />
+      <meshToonMaterial
+        color={parameters.materialColor}
+        gradientMap={gradientTexture}
+      />
     </mesh>
   );
 };
-const Scroll = () => {
+
+const Mesh2: React.FC<MeshProps> = ({
+  gradientTexture,
+  position = [0, 0, 0],
+}) => {
+  return (
+    <mesh position={position}>
+      <coneGeometry args={[1, 2, 32]} />
+      <meshToonMaterial
+        color={parameters.materialColor}
+        gradientMap={gradientTexture}
+      />
+    </mesh>
+  );
+};
+
+const Mesh3: React.FC<MeshProps> = ({
+  gradientTexture,
+  position = [0, 0, 0],
+}) => {
+  return (
+    <mesh position={position}>
+      <torusKnotGeometry args={[0.8, 0.35, 100, 16]} />
+      <meshToonMaterial
+        color={parameters.materialColor}
+        gradientMap={gradientTexture}
+      />
+    </mesh>
+  );
+};
+
+const Meshes = () => {
+  const gradientImagePath: string = "/textures/gradients/3.jpg";
+  const [gradientTexture] = useLoader(TextureLoader, [gradientImagePath]);
+  gradientTexture.magFilter = NearestFilter;
+
+  const objectsDistance: number = 4;
+
   return (
     <>
+      <Mesh1
+        gradientTexture={gradientTexture}
+        position={[0, -objectsDistance * 0, 0]}
+      />
+      <Mesh2
+        gradientTexture={gradientTexture}
+        position={[0, -objectsDistance * 1, 0]}
+      />
+      <Mesh3
+        gradientTexture={gradientTexture}
+        position={[0, -objectsDistance * 2, 0]}
+      />
+    </>
+  );
+};
+
+const Scroll = () => {
+  return (
+    <div style={{ background: "#1e1a20" }}>
       <Canvas
         style={{
           height: "calc(100vh - 96px)",
@@ -20,7 +92,12 @@ const Scroll = () => {
         }}
         camera={{ position: [0, 0, 3] }}
       >
-        <Box />
+        <Meshes />
+        <directionalLight
+          color={"#ffffff"}
+          intensity={3}
+          position={[1, 1, 0]}
+        />
       </Canvas>
       <section className="flex items-center h-[calc(100vh-96px)] relative text-[#ffeded] text-[7vmin] uppercase px-[10%]">
         <h1>My Portfolio</h1>
@@ -31,7 +108,7 @@ const Scroll = () => {
       <section className="flex items-center h-[calc(100vh-96px)] relative text-[#ffeded] text-[7vmin] uppercase px-[10%]">
         <h1>Contact Me</h1>
       </section>
-    </>
+    </div>
   );
 };
 
