fe626af0c037645aa374861d00fc1465336be2d5
Han <hfai97@gmail.com>
2024-07-14 19:32:20 +0800

    Setting the raycaster to change the color of the spheres

--- 19-raycaster/script.ts
+++ 19-raycaster/script.ts
@@ -31,6 +31,18 @@ object3.position.x = 2;
 
 scene.add(object1, object2, object3);
 
+// Raycaster
+const raycaster: THREE.Raycaster = new THREE.Raycaster();
+// const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// const rayDirection = new THREE.Vector3(10, 0, 0);
+// rayDirection.normalize();
+// raycaster.set(rayOrigin, rayDirection);
+
+// const intersect = raycaster.intersectObject(object2);
+// console.log(intersect);
+// const intersects = raycaster.intersectObjects([object1, object2, object3]);
+// console.log(intersects);
+
 // Sizes
 const sizes = {
   width: window.innerWidth,
@@ -58,7 +70,7 @@ const camera: THREE.Camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(0, 0, 2);
+camera.position.set(0, 0, 3);
 scene.add(camera);
 
 // Renderer
@@ -76,7 +88,34 @@ controls.enableDamping = true;
 controls.enablePan = false;
 
 // Animate
+const clock: THREE.Clock = new THREE.Clock();
+
 const tick = () => {
+  const elapsedTime: number = clock.getElapsedTime();
+
+  // Animate objects
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+  // Cast a ray
+  const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  const rayDirection = new THREE.Vector3(1, 0, 0);
+  rayDirection.normalize();
+
+  raycaster.set(rayOrigin, rayDirection);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+
+  for (const object of objectsToTest) {
+    object.material.color.set("#ff0000");
+  }
+
+  for (const intersect of intersects) {
+    intersect.object.material.color.set("#0000ff");
+  }
+
   controls.update();
 
   renderer.render(scene, camera);
