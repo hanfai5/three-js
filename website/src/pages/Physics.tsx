bf66edcf43325a5e049e56409e45a4804b944abb
Han <hfai97@gmail.com>
2024-07-15 20:49:52 +0800

    Added hit sound to the collision of the objects

--- website/src/pages/Physics.tsx
+++ website/src/pages/Physics.tsx
@@ -1,15 +1,35 @@
-import { useSphere, Physics, usePlane, useBox } from "@react-three/cannon";
+import {
+  useSphere,
+  Physics,
+  usePlane,
+  useBox,
+  useContactMaterial,
+} from "@react-three/cannon";
 import { OrbitControls, useTexture } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import GUI from "lil-gui";
 import { useEffect, useRef, useState } from "react";
 import { Mesh } from "three";
+import useSound from "use-sound";
+import hit from "/sounds/hit.mp3";
 
+type PlaySoundFunction = () => void;
 type SphereProps = {
   position: [number, number, number];
+  playSound?: PlaySoundFunction;
+};
+type ContactMaterial = {
+  friction: number;
+  restitution: number;
 };
 
-const Sphere: React.FC<SphereProps> = ({ position }) => {
+const contactMaterial: ContactMaterial = {
+  friction: 0.1,
+  restitution: 0.7,
+};
+
+const Sphere: React.FC<SphereProps> = ({ position, playSound }) => {
+  useContactMaterial("default", "default", contactMaterial);
   const [environmentMapTexture] = useTexture([
     "/textures/environmentMaps/0/px.png",
     "/textures/environmentMaps/0/nx.png",
@@ -24,7 +44,12 @@ const Sphere: React.FC<SphereProps> = ({ position }) => {
       args: [0.5],
       mass: 1,
       position: position,
-      material: { friction: 0.1, restitution: 0.7 },
+      material: "default",
+      onCollide: (e) => {
+        if (playSound && e.contact.impactVelocity > 1.5) {
+          playSound();
+        }
+      },
     }),
     useRef<Mesh>(null!)
   );
@@ -44,9 +69,11 @@ const Sphere: React.FC<SphereProps> = ({ position }) => {
 
 type BoxProps = {
   position: [number, number, number];
+  playSound?: PlaySoundFunction;
 };
 
-const Box: React.FC<BoxProps> = ({ position }) => {
+const Box: React.FC<BoxProps> = ({ position, playSound }) => {
+  useContactMaterial("default", "default", contactMaterial);
   const [environmentMapTexture] = useTexture([
     "/textures/environmentMaps/0/px.png",
     "/textures/environmentMaps/0/nx.png",
@@ -61,7 +88,12 @@ const Box: React.FC<BoxProps> = ({ position }) => {
       args: [1, 1.5, 2],
       mass: 1,
       position: position,
-      material: { friction: 0.1, restitution: 0.7 },
+      material: "default",
+      onCollide: (e) => {
+        if (playSound && e.contact.impactVelocity > 1.5) {
+          playSound();
+        }
+      },
     }),
     useRef<Mesh>(null!)
   );
@@ -80,6 +112,10 @@ const Box: React.FC<BoxProps> = ({ position }) => {
 };
 
 const Floor = () => {
+  useContactMaterial("default", "default", {
+    friction: 0.1,
+    restitution: 0.7,
+  });
   const [environmentMapTexture] = useTexture([
     "/textures/environmentMaps/0/px.png",
     "/textures/environmentMaps/0/nx.png",
@@ -93,7 +129,7 @@ const Floor = () => {
     () => ({
       mass: 0,
       rotation: [-Math.PI * 0.5, 0, 0],
-      material: { friction: 0.1, restitution: 0.7 },
+      material: "default",
     }),
     useRef<Mesh>(null!)
   );
@@ -126,8 +162,13 @@ const Lights = () => {
 };
 
 const App = () => {
-  const [spheres, setSpheres] = useState<SphereProps[]>([]);
-  const [boxes, setBoxes] = useState<BoxProps[]>([]);
+  const [spheres, setSpheres] = useState<
+    { position: [number, number, number] }[]
+  >([]);
+  const [boxes, setBoxes] = useState<{ position: [number, number, number] }[]>(
+    []
+  );
+  const [playSound] = useSound(hit);
 
   useEffect(() => {
     const gui = new GUI();
@@ -137,14 +178,14 @@ const App = () => {
   }, []);
 
   const addNewSphere = () => {
-    const sphere: SphereProps = {
+    const sphere: { position: [number, number, number] } = {
       position: [(Math.random() - 0.5) * 3, 3, (Math.random() - 0.5) * 3],
     };
     setSpheres((prevSpheres) => [...prevSpheres, sphere]);
   };
 
   const addNewBox = () => {
-    const box: BoxProps = {
+    const box: { position: [number, number, number] } = {
       position: [(Math.random() - 0.5) * 3, 3, (Math.random() - 0.5) * 3],
     };
     setBoxes((prevBoxes) => [...prevBoxes, box]);
@@ -154,6 +195,21 @@ const App = () => {
     <section className="max-w-[1200px] mx-auto flex flex-col gap-y-4">
       <h1 className="title">Physics</h1>
 
+      <h2 className="subtitle">Simple Physics</h2>
+      <Canvas
+        style={{ height: "600px" }}
+        camera={{ position: [-3, 3, 3] }}
+        shadows
+      >
+        <Physics gravity={[0, -9.82, 0]}>
+          <Sphere position={[0, 3, 0]} />
+          <Floor />
+        </Physics>
+        <Lights />
+        <OrbitControls enablePan={false} />
+      </Canvas>
+
+      <h2 className="subtitle">Adding Objects and Sounds</h2>
       <Canvas
         style={{ height: "600px" }}
         camera={{ position: [-3, 3, 3] }}
@@ -161,10 +217,10 @@ const App = () => {
       >
         <Physics gravity={[0, -9.82, 0]}>
           {spheres.map(({ position }, i) => (
-            <Sphere key={i} position={position} />
+            <Sphere key={i} position={position} playSound={playSound} />
           ))}
           {boxes.map(({ position }, i) => (
-            <Box key={i} position={position} />
+            <Box key={i} position={position} playSound={playSound} />
           ))}
           <Floor />
         </Physics>
