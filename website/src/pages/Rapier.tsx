31520f6fe1e6e2940e041b614cba5257638ccf31
Han <hfai97@gmail.com>
2024-07-26 21:40:46 +0800

    Added custom colliders from Rapier

--- website/src/pages/Rapier.tsx
+++ website/src/pages/Rapier.tsx
@@ -1,5 +1,12 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import {
+  BallCollider,
+  CuboidCollider,
+  Physics,
+  RigidBody,
+} from "@react-three/rapier";
+import { Perf } from "r3f-perf";
 
 const Lights = () => {
   return (
@@ -28,20 +35,47 @@ const Rapier = () => {
       }}
     >
       <color args={["green"]} attach={"background"} />
+      <Perf position="top-left" />
       <OrbitControls enablePan={false} />
       <Lights />
-      <mesh castShadow position={[-2, 2, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial color={"orange"} />
-      </mesh>
-      <mesh castShadow position={[2, 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"mediumpurple"} />
-      </mesh>
-      <mesh receiveShadow position-y={-1.25}>
-        <boxGeometry args={[10, 0.5, 10]} />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
+
+      <Physics debug>
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[0, 4, 0]}>
+            <sphereGeometry />
+            <meshStandardMaterial color={"orange"} />
+          </mesh>
+        </RigidBody>
+
+        {/* <RigidBody>
+          <mesh castShadow position={[2, 2, 0]}>
+            <boxGeometry args={[3, 2, 1]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+          <mesh castShadow position={[2, 2, 3]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody> */}
+        <RigidBody
+          colliders={false}
+          position={[0, 1, 0]}
+          rotation={[Math.PI * 0.2, 0, 0]}
+        >
+          <BallCollider args={[1.5]} />
+          <mesh castShadow>
+            <torusGeometry args={[1, 0.5, 16, 32]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody type="fixed">
+          <mesh receiveShadow position-y={-1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
+            <meshStandardMaterial color="greenyellow" />
+          </mesh>
+        </RigidBody>
+      </Physics>
     </Canvas>
   );
 };
